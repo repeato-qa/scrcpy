4792ff7d5659036348fe3c5adf005e0cbcbc4fa0
Sergey Volkov <s.volkov@netris.ru>
2020-08-07 13:52:39 +0300

    Store connection id

--- server/src/main/java/com/genymobile/scrcpy/WebSocketConnection.java
+++ server/src/main/java/com/genymobile/scrcpy/WebSocketConnection.java
@@ -1,23 +1,61 @@
 package com.genymobile.scrcpy;
 
 import org.java_websocket.WebSocket;
+import org.java_websocket.framing.CloseFrame;
 import org.java_websocket.handshake.ClientHandshake;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class WebSocketConnection extends Connection implements WSServer.EventsHandler {
     private WSServer wsServer;
-    private int receivingClientsCount = 0;
+    private short receivingClientsCount = 0;
     private static final byte[] MAGIC_BYTES = "scrcpy".getBytes(StandardCharsets.UTF_8);
-    private final byte[] DEVICE_NAME_BYTES = Device.getDeviceName().getBytes(StandardCharsets.UTF_8);
+    private static final byte[] DEVICE_NAME_BYTES = Device.getDeviceName().getBytes(StandardCharsets.UTF_8);
 
     public WebSocketConnection(Options options, VideoSettings videoSettings) {
         super(options, videoSettings);
         wsServer = new WSServer(this, options.getPortNumber());
         wsServer.setReuseAddr(true);
         wsServer.run();
+    }
+
+    private static final class ConnectionInfo {
+        private static final HashSet<Short> INSTANCES_BY_ID = new HashSet<>();
+        private final short id;
+        private boolean isReceiving;
+
+        ConnectionInfo(short id, boolean isReceiving) {
+            this.id = id;
+            this.isReceiving = isReceiving;
+            INSTANCES_BY_ID.add(id);
+        }
+        public static short getNextClientId() {
+            short nextClientId = 0;
+            while (INSTANCES_BY_ID.contains(++nextClientId)) {
+                if (nextClientId == Short.MAX_VALUE) {
+                    return -1;
+                }
+            }
+            return nextClientId;
+        }
+
+        public short getId() {
+            return id;
+        }
+
+        public boolean getIsReceiving() {
+            return isReceiving;
+        }
+
+        public void setIsReceiving(boolean isReceiving) {
+            this.isReceiving = isReceiving;
+        }
+        public void release() {
+            INSTANCES_BY_ID.remove(id);
+        }
     }
 
     public static ByteBuffer deviceMessageToByteBuffer(DeviceMessage msg) {
@@ -34,11 +72,30 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
             return;
         }
         for (WebSocket conn : wsServer.getConnections()) {
-            Boolean streaming = conn.<Boolean>getAttachment();
-            if (conn.isOpen() && streaming != null && streaming) {
+            ConnectionInfo info = conn.getAttachment();
+            if (!conn.isOpen() && info == null) {
+                continue;
+            }
+            if (info.getIsReceiving()) {
                 conn.send(data);
             }
         }
+    }
+
+    private void sendInitialInfoToAll() {
+        Collection<WebSocket> connections = wsServer.getConnections();
+        if (connections.isEmpty()) {
+            return;
+        }
+        for (WebSocket conn : wsServer.getConnections()) {
+            ConnectionInfo info = conn.getAttachment();
+            if (!conn.isOpen() && info == null) {
+                continue;
+            }
+            short clientId = info.getId();
+            conn.send(getInitialInfo(clientId, receivingClientsCount));
+        }
+
     }
 
     public void sendDeviceMessage(DeviceMessage msg) {
@@ -58,23 +115,33 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
 
     public void setVideoSettings(byte[] bytes) {
         super.setVideoSettings(bytes);
-        send(getInitialInfo());
+        sendInitialInfoToAll();
     }
 
     @SuppressWarnings("checkstyle:MagicNumber")
-    protected ByteBuffer getInitialInfo() {
+    protected ByteBuffer getInitialInfo(short clientId, short clientsCount) {
         byte[] screenInfoBytes = device.getScreenInfo().toByteArray();
         byte[] videoSettingsBytes = videoSettings.toByteArray();
 
-        byte[] fullInfo = new byte[MAGIC_BYTES.length + DEVICE_NAME_FIELD_LENGTH +
-                screenInfoBytes.length + videoSettingsBytes.length];
+        byte[] fullInfo = new byte[
+                MAGIC_BYTES.length
+                + DEVICE_NAME_FIELD_LENGTH
+                + screenInfoBytes.length
+                + videoSettingsBytes.length
+                + 2  // short clientId
+                + 2  // short clientsCount
+        ];
         System.arraycopy(MAGIC_BYTES, 0, fullInfo, 0, MAGIC_BYTES.length);
         int len = Math.min(DEVICE_NAME_FIELD_LENGTH - 1, DEVICE_NAME_BYTES.length);
         System.arraycopy(DEVICE_NAME_BYTES, 0, fullInfo, MAGIC_BYTES.length, len);
+        ByteBuffer full = ByteBuffer.wrap(fullInfo);
+        full.position(MAGIC_BYTES.length + len);
         ByteBuffer temp = ByteBuffer.wrap(fullInfo, MAGIC_BYTES.length + DEVICE_NAME_FIELD_LENGTH,
-                screenInfoBytes.length + videoSettingsBytes.length);
+                screenInfoBytes.length + videoSettingsBytes.length + 2 + 2);
         temp.put(screenInfoBytes);
         temp.put(videoSettingsBytes);
+        temp.putShort(clientId);
+        temp.putShort(clientsCount);
         return ByteBuffer.wrap(fullInfo);
     }
 
@@ -83,6 +150,9 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
             Ln.d("No receiving clients.");
             device.setRotationListener(null);
         } else {
+            if (receivingClientsCount == 1 && !device.isScreenOn()) {
+                controller.turnScreenOn();
+            }
             if (screenEncoder == null || !screenEncoder.isAlive()) {
                 Ln.d("New connection while encoder is dead.");
                 device.setRotationListener(this);
@@ -95,7 +165,14 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         if (conn.isOpen()) {
-            conn.send(getInitialInfo());
+            short clientId = ConnectionInfo.getNextClientId();
+            if (clientId == -1) {
+                conn.close(CloseFrame.TRY_AGAIN_LATER);
+                return;
+            }
+            ConnectionInfo info = new ConnectionInfo(clientId, false);
+            conn.setAttachment(info);
+            conn.send(getInitialInfo(clientId, receivingClientsCount));
             Ln.d("Client entered the room!");
             if (this.streamInvalidateListener != null) {
                 streamInvalidateListener.onStreamInvalidate();
@@ -108,7 +185,12 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
         Ln.d("Client has left the room!");
         FilePushHandler.cancelAllForConnection(conn);
         receivingClientsCount--;
+        ConnectionInfo info = conn.getAttachment();
+        if (info != null) {
+            info.release();
+        }
         checkConnectionsCount();
+        sendInitialInfoToAll();
     }
 
     @Override
@@ -126,19 +208,20 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
                 FilePushHandler.handlePush(conn, controlMessage);
                 return;
             }
-            controller.handleEvent(controlMessage);
             if (controlMessage.getType() == ControlMessage.TYPE_CHANGE_STREAM_PARAMETERS) {
-                Boolean streaming = conn.<Boolean>getAttachment();
-                if (streaming == null || !streaming) {
-                    conn.send(getInitialInfo());
-                    conn.setAttachment(true);
+                ConnectionInfo info = conn.getAttachment();
+                if (info == null) {
+                    Ln.e("No info attached to connection");
+                    return;
+                }
+                if (!info.getIsReceiving()) {
+                    info.setIsReceiving(true);
                     receivingClientsCount++;
-                    if (receivingClientsCount == 1 && !device.isScreenOn()) {
-                        controller.turnScreenOn();
-                    }
                     checkConnectionsCount();
                 }
             }
+            controller.handleEvent(controlMessage);
+
         } else {
             Ln.d("-1 " + address + ": " + message);
         }
@@ -162,6 +245,6 @@ public class WebSocketConnection extends Connection implements WSServer.EventsHa
 
     public void onRotationChanged(int rotation) {
         super.onRotationChanged(rotation);
-        send(getInitialInfo());
+        sendInitialInfoToAll();
     }
 }
